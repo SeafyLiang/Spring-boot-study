f4738752c68c1db94ef8bba042900e07f2f3bced
SeafyLiang <894700104@qq.com>
2020-03-09 23:22:41 +0800

    spring-boot-web-crud

---
+++ src/test/java/com/seafy/springbootwebrestfulcrud/SpringBootWebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seafy.springbootwebrestfulcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebRestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
